c599720c4c2e405beaf28305b311825d616af3b3
Itay Dafna <i.b.dafna@gmail.com>
2021-01-10 21:29:09 -0800

    clean up and refactor

--- src/flask.ts
+++ src/flask.ts
@@ -1,24 +1,39 @@
-// Setting up a custom fecth function
-const originalFetch = window.fetch;
 
+/**
+ * A small, virtual web server emulating Flask (Python).
+ */
+declare var pyodide: any;
 const router = {
   "/_dash-layout": () => "app.serve_layout()",
   "/_dash-dependencies": () => `
-with app.server.app_context(): 
-  x = app.dependencies()
-x`,
+      with app.server.app_context(): 
+        x = app.dependencies()
+      x`,
   "/_dash-update-component": postRequest,
   "/_reload-hash": () => "app.serve_reload_hash()"
 }
 
+/**
+ * Sends a POST request to the Python Flask backend.
+ * @param req Request Object
+ * @param init request payload
+ */
 function postRequest(req, init) {
   console.log('[POST Request]', req, init);
   return `
-with app.server.test_request_context('${req}', data='''${init.body}''', content_type="application/json"): 
-  x = app.dispatch()
-x`
+    with app.server.test_request_context('${req}', 
+      data='''${init.body}''', 
+      content_type="application/json"): 
+      x = app.dispatch()
+    x`
 }
 
+
+/**
+ * Retrieves a Flask response object and converts it
+ * to a compatible Response object.
+ * @param codeWillRun stringified python code
+ */
 function generateResponse(codeWillRun) {
   console.log("[Pyodide Request]");
   const flaskRespone = pyodide.runPython(codeWillRun);
@@ -30,6 +45,12 @@ function generateResponse(codeWillRun) {
   return response;
 }
 
+/**
+ * A custom fetch function which intercepts Flask requests
+ * and routes to the Python backend.
+ * @param req Request object
+ * @param init request payload
+ */
 async function fetch(
     req: Request,
     init?: RequestInit | null | undefined
@@ -45,10 +66,10 @@ async function fetch(
     }
 
      else {
-      console.log("[Regular Reuqest]")
+      console.log("[Passthrough Reuqest]")
       return originalFetch.apply(this, [req, init]);
     }
   }
 
-//@ts-ignore
+const originalFetch = window.fetch;
 window.fetch = fetch;
