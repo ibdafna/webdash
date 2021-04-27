5b83e7f912ba845c9e955812f835aa1add622128
Itay Dafna <i.b.dafna@gmail.com>
2021-04-26 20:16:16 -0700

    Add more types and docstrings
    Signed-off-by: Itay Dafna <i.b.dafna@gmail.com>
    
    Former-commit-id: 024c8c47148d2ac3e730fdf1f4528670de892cd6 [formerly 87f094472df28b0ec80eda5fe1d1b08be8e5a7b3]
    Former-commit-id: 5355b121b682be7c3845d8fb77958568aa37467c
    Former-commit-id: 944cd815426426fda8caff1aa8231ed8bdbcfaba
    Former-commit-id: 5977402bb5ca10af29d1d5d32e9b2612faa1c868

--- src/worker-loader.ts
+++ src/worker-loader.ts
@@ -1,8 +1,18 @@
+import { ErrorCallback } from "typescript";
+
+/**
+ * A small queue where we queue "OnSuccess" functions
+ * for each request sent. For each request, a success
+ * function gets queued. Upon each incoming message,
+ * we dequeue and execute the "OnSuccess function".
+ *
+ * TODO: Implement a more efficient queuing system.
+ */
 class ResponseQueue {
   constructor() {
     this.queue = [];
   }
-  enqueue(onSuccessFn) {
+  enqueue(onSuccessFn: Function) {
     return this.queue.push(onSuccessFn);
   }
   dequeue() {
@@ -12,58 +22,85 @@ class ResponseQueue {
   queue: Array<Function>;
 }
 
+/**
+ * This class is used as an intermediary between
+ * the WebWorker and the application client.
+ * It currently supports two main message types:
+ *
+ *  1. HTTP Request/Response objects.
+ *  2. binary file transfers.
+ */
 export class WorkerManager {
   constructor() {
     this.queue = new ResponseQueue();
     this.worker = new Worker("./worker.js");
   }
 
-  run(script, context, onSuccess, onError): void {
+  run(
+    script: string,
+    context: Payload,
+    onSuccess: Function,
+    onError: Function
+  ): void {
     this.queue.enqueue(onSuccess);
-    this.worker.onerror = e => onError(e);
-    this.worker.onmessage = e => {
+    this.worker.onerror = (e) => onError(e);
+    this.worker.onmessage = (e) => {
       console.log("[4. Message received from worker]", e.data);
       const success = this.queue.dequeue();
       return success(e.data.results);
     };
     this.worker.postMessage({
       ...context,
-      python: script
+      python: script,
     });
   }
 
-  asyncRun(script, context): Promise<any> {
-    return new Promise((onSuccess, onError) => {
-      this.run(script, context, onSuccess, onError);
-    });
+  /**
+   * Runs a Python script on the WebWorker
+   * @param script python script to run
+   * @param context optional additional arguments
+   * @returns BlobPart or string
+   */
+  asyncRun(script: string, context: Payload): Promise<BlobPart | string> {
+    return new Promise(
+      (onSuccess: Function, onError: (e: ErrorEvent) => any) => {
+        this.run(script, context, onSuccess, onError);
+      }
+    );
   }
 
-  async fsReadDir(dir): Promise<any> {
-    return new Promise((onSuccess, onError) => {
-      this.worker.onerror = onError;
-      this.worker.onmessage = (e: MessageEvent) => onSuccess(e.data.results);
-      this.worker.postMessage({
-        fsCommands: {
-          msgType: "readdir",
-          param: dir
-        }
-      });
-    });
+  async fsReadDir(dir: string): Promise<string> {
+    return new Promise(
+      (onSuccess: Function, onError: (e: ErrorEvent) => any) => {
+        this.worker.onerror = onError;
+        this.worker.onmessage = (e: MessageEvent) => onSuccess(e.data.results);
+        this.worker.postMessage({
+          fsCommands: {
+            msgType: "readdir",
+            param: dir,
+          },
+        });
+      }
+    );
   }
 
-  async fsReadFile(file): Promise<any> {
-    return new Promise((onSuccess, onError) => {
-      this.worker.onerror = onError;
-      this.worker.onmessage = (e: MessageEvent) => onSuccess(e.data.results);
-      this.worker.postMessage({
-        fsCommands: {
-          msgType: "readFile",
-          param: file
-        }
-      });
-    });
+  async fsReadFile(file: string): Promise<BlobPart | string> {
+    return new Promise(
+      (onSuccess: Function, onError: (e: ErrorEvent) => any) => {
+        this.worker.onerror = onError;
+        this.worker.onmessage = (e: MessageEvent) => onSuccess(e.data.results);
+        this.worker.postMessage({
+          fsCommands: {
+            msgType: "readFile",
+            param: file,
+          },
+        });
+      }
+    );
   }
 
   queue: ResponseQueue;
   worker: Worker;
 }
+
+type Payload = { [key: string]: any };
